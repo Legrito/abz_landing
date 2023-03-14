36bbfcac642673bce17a0af557f5713a3a9591c8
Lesya <jamma@i.ua>
2023-03-14 13:58:08 +0100

    Update Button component

--- src/components/Shared/Button.js
+++ src/components/Shared/Button.js
@@ -1,9 +1,14 @@
 import styles from "./Button.module.sass";
 
-const Button = ({ path, children }) => (
-  <a className={styles.button} href={path}>
-    {children}
-  </a>
-);
+const Button = ({ path, type = "button", onClick, children }) =>
+  path ? (
+    <a className={styles.button} href={path}>
+      {children}
+    </a>
+  ) : (
+    <button className={styles.button} type={type} onClick={onClick}>
+      {children}
+    </button>
+  );
 
 export default Button;
